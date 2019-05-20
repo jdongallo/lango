59aaf04c8fc6fbbd1faf70af92a2fb6ed2f27d32
Parsa Mogharabin <parsa.mogharabin@gmail.com>
2019-05-20 15:42:15 -0700

    Update lango.jsx

--- public/lango.jsx
+++ public/lango.jsx
@@ -1,145 +1,59 @@
-function Square(props) {
-    return (
-      <button className="square" onClick={props.onClick}>
-        {props.value}
-      </button>
-    );
+'use strict'
+
+function Card(props) {
+    return <div className="textCard">
+         {props.children}
+  </div>;
   }
   
-  class Board extends React.Component {
-    renderSquare(i) {
-      return (
-        <Square
-          value={this.props.squares[i]}
-          onClick={() => this.props.onClick(i)}
-        />
-      );
-    }
-  
-    render() {
-      return (
-        <div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  class Game extends React.Component {
-    constructor(props) {
+
+function Txt(props) {
+   if (props.phrase == undefined) {
+      return <p>Text missing</p>;
+      }
+   else return <p>{props.phrase}</p>;
+   }
+
+
+class CreateCardMain extends React.Component {
+
+  constructor(props) {
       super(props);
-      this.state = {
-        history: [
-          {
-            squares: Array(9).fill(null)
-          }
-        ],
-        stepNumber: 0,
-        xIsNext: true
-      };
-    }
-  
-    handleClick(i) {
-      const history = this.state.history.slice(0, this.state.stepNumber + 1);
-      const current = history[history.length - 1];
-      const squares = current.squares.slice();
-      if (calculateWinner(squares) || squares[i]) {
-        return;
+      this.state = { opinion: "Life is a bowl of cherries" }
+
+      this.checkReturn = this.checkReturn.bind(this);
       }
-      squares[i] = this.state.xIsNext ? "X" : "O";
-      this.setState({
-        history: history.concat([
-          {
-            squares: squares
-          }
-        ]),
-        stepNumber: history.length,
-        xIsNext: !this.state.xIsNext
-      });
-    }
-  
-    jumpTo(step) {
-      this.setState({
-        stepNumber: step,
-        xIsNext: (step % 2) === 0
-      });
-    }
-  
-    render() {
-      const history = this.state.history;
-      const current = history[this.state.stepNumber];
-      const winner = calculateWinner(current.squares);
-  
-      const moves = history.map((step, move) => {
-        const desc = move ?
-          'Go to move #' + move :
-          'Go to game start';
-        return (
-          <li key={move}>
-            <button onClick={() => this.jumpTo(move)}>{desc}</button>
-          </li>
-        );
-      });
-  
-      let status;
-      if (winner) {
-        status = "Winner: " + winner;
-      } else {
-        status = "Next player: " + (this.state.xIsNext ? "X" : "O");
-      }
-  
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board
-              squares={current.squares}
-              onClick={i => this.handleClick(i)}
-            />
-          </div>
-          <div className="game-info">
-            <div>{status}</div>
-            <ol>{moves}</ol>
-          </div>
-        </div>
+
+  render() {return (
+      <main>
+      <Card>
+         <textarea id="inputEng" onKeyPress={this.checkReturn} />
+      </Card>
+      <Card>
+          <Txt phrase={this.state.opinion} /> 
+      </Card>
+      </main>
       );
-    }
-  }
-  
-  // ========================================
-  
-  ReactDOM.render(<Game />, document.getElementById("root"));
-  
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
+    } // end of render function   
+
+    // onKeyPress function for the textarea element
+    // When the charCode is 13, the user has hit the return key
+    checkReturn(event) {
+   if (event.charCode == 13) {
+      let newPhrase = document.getElementById("inputEng").value;
+      this.setState({opinion: newPhrase} );
       }
-    }
-    return null;
-  }
-  
+   }
+
+
+  } // end of class
+
+
+
+ReactDOM.render(
+    <CreateCardMain />,
+    document.getElementById('root')
+);
+
+
+   
