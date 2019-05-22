cfda2ebb065a8612f55c157eb625c5f54141b884
Joseph Dongallo <jdongallo925@gmail.com>
2019-05-22 11:01:28 -0700

    Got translation query working

--- public/lango.js
+++ public/lango.js
@@ -8,6 +8,17 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+var object = undefined;
+var input = document.getElementById("word");
+var engText = ""; // Passed to saveInput function 
+var transText = ""; // to store in database
+
+function createCORSRequest(method, url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+    return xhr;
+}
+
 function Card(props) {
     return React.createElement(
         "div",
@@ -50,7 +61,7 @@ function MakeHeader() {
 function MakeFooter(props) {
     return React.createElement(
         "footer",
-        { className: "footy" },
+        null,
         React.createElement(
             "span",
             null,
@@ -67,9 +78,9 @@ var CreateCardMain = function (_React$Component) {
 
         var _this = _possibleConstructorReturn(this, (CreateCardMain.__proto__ || Object.getPrototypeOf(CreateCardMain)).call(this, props));
 
-        _this.state = { opinion: "Life is a bowl of cherries" };
+        _this.state = { translation: "Life is a bowl of cherries" };
 
-        _this.checkReturn = _this.checkReturn.bind(_this);
+        _this.getTranslation = _this.getTranslation.bind(_this);
         return _this;
     }
 
@@ -77,8 +88,8 @@ var CreateCardMain = function (_React$Component) {
         key: "render",
         value: function render() {
             return React.createElement(
-                "div",
-                { className: "insideBody" },
+                React.Fragment,
+                null,
                 React.createElement(MakeHeader, null),
                 React.createElement(
                     "main",
@@ -86,12 +97,12 @@ var CreateCardMain = function (_React$Component) {
                     React.createElement(
                         Card,
                         null,
-                        React.createElement("textarea", { id: "inputEng", onKeyPress: this.checkReturn })
+                        React.createElement("textarea", { id: "inputEng", onKeyPress: this.getTranslation })
                     ),
                     React.createElement(
                         Card,
                         null,
-                        React.createElement(Txt, { phrase: this.state.opinion })
+                        React.createElement(Txt, { phrase: this.state.translation })
                     )
                 ),
                 React.createElement(MakeFooter, null)
@@ -100,13 +111,41 @@ var CreateCardMain = function (_React$Component) {
 
         // onKeyPress function for the textarea element
         // When the charCode is 13, the user has hit the return key
+        //   checkReturn(event) {
+        //  if (event.charCode == 13) {
+        //     let newPhrase = document.getElementById("inputEng").value;
+        //     this.setState({opinion: newPhrase} );
+        //     }
+        //  }
 
     }, {
-        key: "checkReturn",
-        value: function checkReturn(event) {
+        key: "getTranslation",
+        value: function getTranslation(event) {
             if (event.charCode == 13) {
-                var newPhrase = document.getElementById("inputEng").value;
-                this.setState({ opinion: newPhrase });
+                var url = "/translate?english=" + document.getElementById("inputEng").value;
+                var xhr = createCORSRequest('GET', url);
+
+                if (!xhr) {
+                    alert('CORS not supported');
+                    return;
+                }
+
+                xhr.onload = function () {
+                    var responseStr = xhr.responseText;
+                    object = JSON.parse(responseStr);
+                    console.log(object);
+                    // let output = document.getElementById("outputDiv");
+                    // output.textContent = object.Spanish;
+                    this.setState({ translation: object.Spanish });
+                    engText = object.English;
+                    transText = object.Spanish;
+                }.bind(this);
+
+                xhr.onerror = function () {
+                    alert('There was an error in making the request');
+                };
+
+                xhr.send();
             }
         }
     }]);
--- public/lango.jsx
+++ public/lango.jsx
@@ -1,5 +1,16 @@
 'use strict'
 
+let object = undefined;
+const input = document.getElementById("word");
+let engText = ""; // Passed to saveInput function 
+let transText = ""; // to store in database
+
+function createCORSRequest(method, url) {
+    let xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+    return xhr;
+}
+
 function Card(props) {
     return <div className="textCard">
          {props.children}
@@ -25,7 +36,7 @@ function MakeHeader(){
 
 function MakeFooter(props) {
   return(
-    <footer className="footy">
+    <footer>
       <span> username </span> 
      </footer> 
     );
@@ -36,38 +47,66 @@ class CreateCardMain extends React.Component {
 
   constructor(props) {
       super(props);
-      this.state = { opinion: "Life is a bowl of cherries" }
+      this.state = { translation: "Life is a bowl of cherries" }
 
-      this.checkReturn = this.checkReturn.bind(this);
+      this.getTranslation = this.getTranslation.bind(this);
       }
 
   render() {return (
-      <div className="insideBody">
+      <React.Fragment>
       <MakeHeader />
       <main>
       <Card>
-         <textarea id="inputEng" onKeyPress={this.checkReturn} />
+         <textarea id="inputEng" onKeyPress={this.getTranslation} />
       </Card>
       <Card>
-          <Txt phrase={this.state.opinion} /> 
+          <Txt phrase={this.state.translation} /> 
       </Card>
       </main>
       <MakeFooter />
-      </div>
+      </React.Fragment>
       );
     } // end of render function   
 
     // onKeyPress function for the textarea element
     // When the charCode is 13, the user has hit the return key
-    checkReturn(event) {
-   if (event.charCode == 13) {
-      let newPhrase = document.getElementById("inputEng").value;
-      this.setState({opinion: newPhrase} );
-      }
-   }
+  //   checkReturn(event) {
+  //  if (event.charCode == 13) {
+  //     let newPhrase = document.getElementById("inputEng").value;
+  //     this.setState({opinion: newPhrase} );
+  //     }
+  //  }
 
+  getTranslation(event) {
+    if (event.charCode == 13) {
+        let url = "/translate?english=" + document.getElementById("inputEng").value;
+        let xhr = createCORSRequest('GET', url);
+    
+        if (!xhr) {
+            alert('CORS not supported');
+            return;
+        }
+    
+        xhr.onload = function() {
+            let responseStr = xhr.responseText;
+            object = JSON.parse(responseStr);
+            console.log(object);
+            // let output = document.getElementById("outputDiv");
+            // output.textContent = object.Spanish;
+            this.setState({translation: object.Spanish});
+            engText = object.English;
+            transText = object.Spanish;
+        }.bind(this)
+    
+        xhr.onerror = function() {
+            alert('There was an error in making the request');
+        }
+    
+        xhr.send();
+    }
+  }
 
-  } // end of class
+} // end of class
 
 
 
