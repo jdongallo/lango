957ab5bf968adc0e84eccbeb7864cb5215ce3bd6
Joseph Dongallo <jdongallo925@gmail.com>
2019-06-06 23:04:18 -0700

    Fixed card display

--- src/review.jsx
+++ src/review.jsx
@@ -4,7 +4,6 @@ let object = undefined;
 const input = document.getElementById("word");
 let engText = ""; // Used in saveInput function 
 let transText = ""; // to store in database
-let flashcards = [];
 
 function createCORSRequest(method, url) {
     let xhr = new XMLHttpRequest();
@@ -43,38 +42,76 @@ class CardTextarea extends React.Component {
 class Card extends React.Component {
   constructor(props){
     super(props);
+    this.state = {english: "", spanish: ""};
+    this.nextCard = this.nextCard.bind(this);
+    this.getFlashcards = this.getFlashcards.bind(this);
+    this.flashcards = [];
   }
   render() {
     return(
       <div  id="flipper" className='card-container textCard'>
         <div className='card-body'>
-          <CardBack text="Correct english translation" />
+          <CardBack text={this.state.english} />
 
-          <CardFront text="Spanish stored flashcard" />  
+          <CardFront text={this.state.spanish} />  
         </div>
       </div>
     )
   }
 
-  // nextCard(){
-  //   while (true) {
-  //     let index = Math.floor(Math.random() * flashcards.length);
-  //     let correct = flashcards[index].numCorrect;
-  //     let seen = flashcards[index].numShown;
-  //     score = Math.max(1, 5-correct) + Math.max(1, 5-seen);
-  //     if (seen == 0) {
-  //       score += 5;
-  //     }
-  //     else {
-  //       score += 5 * ( Math.floor((seen-correct)/seen) );
-  //     }
-  //     let randNum = Math.floor(Math.random() * 16);
-  //     if (randNum <= score) {
-  //       this.currCard = flashcards[index].engText;
-  //       break;
-  //     }
-  //   }
-  // }
+  nextCard(){
+    // while (true) {
+    //   let index = Math.floor(Math.random() * this.flashcards.length);
+    //   let correct = this.flashcards[index].numCorrect;
+    //   let seen = this.flashcards[index].numShown;
+    //   let eng = this.flashcards[index].engText;
+    //   let es = this.flashcards[index].transText
+    //   score = Math.max(1, 5-correct) + Math.max(1, 5-seen);
+    //   if (seen == 0) {
+    //     score += 5;
+    //   }
+    //   else {
+    //     score += 5 * ( Math.floor((seen-correct)/seen) );
+    //   }
+    //   let randNum = Math.floor(Math.random() * 16);
+    //   if (randNum <= score) {
+    //     this.setState({english: eng, spanish: es});
+    //     break;
+    //   }
+    // }
+    console.log(this.flashcards[0]);
+  }
+
+  getFlashcards() {
+    let url = "/flashcards";
+    let xhr = createCORSRequest('GET', url);
+
+    if (!xhr) {
+        alert('CORS not supported');
+        return;
+    }
+
+    xhr.onload = function() {
+        this.flashcards = JSON.parse(xhr.responseText);
+        console.log(this.flashcards);
+        let eng = this.flashcards[0].engText;
+        let es = this.flashcards[0].transText;
+        console.log(eng, es);
+        console.log(this.flashcards[0]);
+        this.setState({english: eng, spanish: es}); 
+    }.bind(this)
+
+    xhr.onerror = function() {
+        alert('There was an error in making the request');
+    }.bind(this)
+
+    xhr.send();
+  }
+
+  componentDidMount(){
+    this.getFlashcards();
+    this.nextCard();
+  }
 
 }
 
@@ -216,64 +253,8 @@ class CreateCardMain extends React.Component {
       <MakeFooter />
       </React.Fragment>
       );
-    } // end of render function   
-    // <textarea id="inputEng" onKeyPress={this.getTranslation} />
-    // onKeyPress function for the textarea element
-    // When the charCode is 13, the user has hit the return key
-  //   checkReturn(event) {
-  //  if (event.charCode == 13) {
-  //     let newPhrase = document.getElementById("inputEng").value;
-  //     this.setState({opinion: newPhrase} );
-  //     }
-  //  }
-
-
-  // nextCard(){
-  //   while (true) {
-  //     let index = Math.floor(Math.random() * flashcards.length);
-  //     let correct = flashcards[index].numCorrect;
-  //     let seen = flashcards[index].numShown;
-  //     score = Math.max(1, 5-correct) + Math.max(1, 5-seen);
-  //     if (seen == 0) {
-  //       score += 5;
-  //     }
-  //     else {
-  //       score += 5 * ( Math.floor((seen-correct)/seen) );
-  //     }
-  //     let randNum = Math.floor(Math.random() * 16);
-  //     if (randNum <= score) {
-  //       this.currCard = flashcards[index].engText;
-  //       break;
-  //     }
-  //   }
-  // }
-
+    } 
 } // end of class
-
-
-
-function getFlashcards() {
-      let url = "/flashcards";
-      let xhr = createCORSRequest('GET', url);
-  
-      if (!xhr) {
-          alert('CORS not supported');
-          return;
-      }
-  
-      xhr.onload = function() {
-          flashcards = xhr.responseText;
-          console.log(flashcards);
-      }
-  
-      xhr.onerror = function() {
-          alert('There was an error in making the request');
-      }
-  
-      xhr.send();
-}
-
-getFlashcards();
 
 ReactDOM.render(
     <CreateCardMain />,
